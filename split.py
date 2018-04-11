ce85aa9fd4027cae4f5879287f6730a3e710e219
benlove <benuklove@gmail.com>
2018-04-10 22:30:55 -0500

    Beginning framework for train-valid-test file splits

--- split.py
+++ split.py
@@ -1,2 +1,21 @@
-# before running split.py, make sure there are no unwanted files in image dataset
+# before running split.py, make sure:
+# 1. no unwanted files in image dataset (erroneous, corrupted, etc)
+# 2. equal number of images in each folder
 
+import os, shutil
+
+
+origin = "data/testing/"
+moveto = "data/donetesting/"
+
+if not os.path.exists(origin):
+    os.makedirs(origin)
+if not os.path.exists(moveto):
+    os.makedirs(moveto)
+
+files = os.listdir(origin)
+files.sort()
+for f in files:
+    src = origin+f
+    dst = moveto+f
+    shutil.move(src,dst)
